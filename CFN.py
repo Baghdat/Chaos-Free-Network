6e9fda2e2f8be6a870eb8366eccb1eb38737c150
Baghdat <s1153100@sms.ed.ac.uk>
2018-07-16 11:08:37 +0600

    Update CFN.py

--- CFN.py
+++ CFN.py
@@ -140,50 +140,6 @@ class CFN_RNNCell(tf.nn.rnn_cell.RNNCell):
         output = theta*tf.tanh(state) + eta*tf.tanh(Wx)
     return output, output
 
-'''
-class GRUCell(RNNCell):
-  """Gated Recurrent Unit cell (cf. http://arxiv.org/abs/1406.1078)."""
-
-  def __init__(self,
-               num_units,
-               activation=None,
-               reuse=None,
-               kernel_initializer=None,
-               bias_initializer=None):
-    super(GRUCell, self).__init__(_reuse=reuse)
-    self._num_units = num_units
-    self._activation = activation or math_ops.tanh
-    self._kernel_initializer = kernel_initializer
-    self._bias_initializer = bias_initializer
-
-  @property
-  def state_size(self):
-    return self._num_units
-
-  @property
-  def output_size(self):
-    return self._num_units
-
-  def call(self, inputs, state):
-    """Gated recurrent unit (GRU) with nunits cells."""
-    with vs.variable_scope("gates"):  # Reset gate and update gate.
-      # We start with bias of 1.0 to not reset and not update.
-      bias_ones = self._bias_initializer
-      if self._bias_initializer is None:
-        dtype = [a.dtype for a in [inputs, state]][0]
-        bias_ones = init_ops.constant_initializer(1.0, dtype=dtype)
-      value = math_ops.sigmoid(
-          _linear([inputs, state], 2 * self._num_units, True, bias_ones,
-                  self._kernel_initializer))
-      r, u = array_ops.split(value=value, num_or_size_splits=2, axis=1)
-    with vs.variable_scope("candidate"):
-      c = self._activation(
-          _linear([inputs, r * state], self._num_units, True,
-                  self._bias_initializer, self._kernel_initializer))
-    new_h = u * state + (1 - u) * c
-    return new_h, new_h
-'''
-
 #_LSTMStateTuple = collections.namedtuple("LSTMStateTuple", ("c", "h"))
 
 
